3f158554feb8e9b9446bae67e949c45d84dc2169
susan-kimemia <susankimemia111@gmail.com>
2025-10-29 09:55:58 +0300

    Create Header, maincontent, & footer Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
